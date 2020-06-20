8d83f91da711bfe5091fd5992077e859d3b04e13
Ana Nikolova <ani.nik19@gmail.com>
2020-06-20 11:11:18 +0200

    create post page

--- app/routes.js
+++ app/routes.js
@@ -9,4 +9,8 @@ router.get("/generic", (req, res) => {
   return res.render("../assets/views/generic.pug");
 });
 
+router.get("/post/create", (req, res) => {
+  return res.render("../assets/views/post/create.pug");
+});
+
 module.exports = router;

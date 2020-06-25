c5a6f760e886a445173b3103d0fa043340a2464f
Ana Nikolova <ani.nik19@gmail.com>
2020-06-25 12:14:27 +0200

    edit post

--- app/routes.js
+++ app/routes.js
@@ -49,4 +49,44 @@ router.get("/post/:id", (req, res) => {
   );
 });
 
+router.get("/post/:id/edit", (req, res) => {
+  DB.query(
+    `SELECT * FROM posts WHERE id = ${req.params.id} LIMIT 1 `,
+    (error, result) => {
+      if (error) {
+        console.log("error");
+        console.log(error);
+        return res.redirect("/");
+      } else {
+        console.log("results:");
+        console.log(result[0]);
+        return res.render("../assets/views/post/edit.pug", {
+          id: req.params.id,
+          title: result[0].title,
+          description: result[0].description,
+          image_url: result[0].image_url,
+        });
+      }
+    }
+  );
+});
+
+router.post("/post/:id/edit", (req, res) => {
+  const post = req.body;
+  DB.query(
+    `UPDATE posts SET 
+    title='${post.title}', 
+    image_url='${post.image_url}',
+    description='${post.description}' 
+    WHERE id=${req.params.id}`,
+    (error, result) => {
+      if (error) {
+        return res.redirect("/post/create");
+      } else {
+        return res.redirect(`/post/${req.params.id}`);
+      }
+    }
+  );
+});
+
 module.exports = router;
